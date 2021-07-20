e7f5e3106d52c3a9fe4df83815cdc11065e4ba42
Bharat <jainb0206@gmail.com>
2021-07-20 14:32:11 +0530

    heroku deploy

--- models/secrets.js
+++ models/secrets.js
@@ -1,3 +1,3 @@
 const cryptoJs = require('crypto-js');
-const on = cryptoJs.SHA256("process.env.HIDDEN_KEY").toString();
+const on = cryptoJs.SHA256("sdfjh2hu3u92873883YUIY3U").toString();
 module.exports = on;
