4b153f90d93dc7a9294df8652083ec172539ff77
Bharat <jainb0206@gmail.com>
2021-07-20 14:26:41 +0530

    heroku deploy

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,7 @@
 // require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect("process.env.MONGO_CONNECTION_URL", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }).then(() => {
+mongoose.connect("mongodb+srv://ChromeCorp:QWErty@765@cluster0.efepk.mongodb.net/FileSharing?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }).then(() => {
     console.log('DataBase Connected!');
 }).catch((error => {
     console.log('Connection failed: ' + error);
--- mailsender/mailsender.js
+++ mailsender/mailsender.js
@@ -7,7 +7,7 @@ function mailsend(user, key) {
         service: 'gmail',
         auth: {
             user: 'OrgaChrdomeHms23@gmail.com',
-            pass: "QWErty@765"
+            pass: 'QWErty@765'
         }
     });
     var mailOptions = {
